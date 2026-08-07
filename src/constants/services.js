e925a3894faf5068769339e6d86e1438505418a9
Mohit Ranjan <mohitranjan2111@gmail.com>
2026-08-08 00:35:58 +0530

    feat: implement Services section component and add associated assets and data constants

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,37 +1,38 @@
+import imgEnvironmentalMonitoring from '../assets/images/homePage/serviceSection/Environmental-Monitoring.avif';
+import imgLaboratoryServices from '../assets/images/homePage/serviceSection/Laboratory-Services.jpeg';
+import imgConsultancy from '../assets/images/homePage/serviceSection/Consultancy.jpeg';
+import imgFireSafetyAudits from '../assets/images/homePage/serviceSection/Fire-Safety-Audits.jpeg';
+import imgWasteManagement from '../assets/images/homePage/serviceSection/Waste-Management.avif';
+
 export const services = [
   {
     id: 1,
     title: "Environmental Monitoring",
     description: "Real-time monitoring of air, water, and soil quality to ensure compliance and environmental health.",
-    icon: "Activity",
-    color: "bg-brand-blue"
+    image: imgEnvironmentalMonitoring
   },
   {
     id: 2,
     title: "Laboratory Services",
     description: "NABL-accredited laboratory offering comprehensive testing for ambient air, water, soil, and emission parameters.",
-    icon: "Beaker",
-    color: "bg-brand-green"
+    image: imgLaboratoryServices
   },
   {
     id: 3,
     title: "Consultancy",
     description: "Expert environmental consulting for EIA, compliance audits, policy planning, and sustainable solutions.",
-    icon: "Lightbulb",
-    color: "bg-brand-orange"
+    image: imgConsultancy
   },
   {
     id: 4,
-    title: "Compliance Services",
-    description: "Navigate complex environmental regulations with our statutory compliance and legal advisory services.",
-    icon: "CheckCircle",
-    color: "bg-brand-blue-dark"
+    title: "Fire And Safety Compliance",
+    description: "Navigate complex fire and safety regulations with our statutory compliance and fire and safety advisory services.",
+    image: imgFireSafetyAudits
   },
   {
     id: 5,
     title: "Waste Management",
     description: "Sustainable waste management solutions including solid waste, hazardous waste, and e-waste handling.",
-    icon: "Trash2",
-    color: "bg-brand-green-dark"
+    image: imgWasteManagement
   }
 ];
