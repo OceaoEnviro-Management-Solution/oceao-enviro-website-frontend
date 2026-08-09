859bff0c231631fc893cba44504ebceb4ee40c57
Mohit Ranjan <mohitranjan2111@gmail.com>
2026-08-09 17:29:50 +0530

    feat: add Certifications section with PDF preview and lightbox support

--- src/constants/certifications.js
+++ src/constants/certifications.js
@@ -2,19 +2,19 @@ export const certifications = [
   {
     id: 1,
     name: "QCI - NABET Accreditation",
-    image: "/src/assets/images/certifications/qci-nabet.png",
+    pdfUrl: "/src/assets/images/certifications/QCI-NABET.pdf",
     alt: "QCI NABET Certificate"
   },
   {
     id: 2,
     name: "ISO 14001:2015 Certification",
-    image: "/src/assets/images/certifications/iso-14001.png",
+    pdfUrl: "/src/assets/images/certifications/ISO-14001.pdf",
     alt: "ISO 14001 Environmental Management Certificate"
   },
   {
     id: 3,
     name: "ISO 45001:2018 Certification",
-    image: "/src/assets/images/certifications/iso-45001.png",
+    pdfUrl: "/src/assets/images/certifications/ISO-45001.pdf",
     alt: "ISO 45001 Occupational Health & Safety Certificate"
   }
 ];
